a3e0234ba5683d9443e49311ff2315b24e65e1df
Vladimir Vasilenko <9319669@gmail.com>
2023-06-12 00:40:12 +0300

    Datatest.csv added

--- src/test/java/ru/netology/services/NumberMonthRestTest.java
+++ src/test/java/ru/netology/services/NumberMonthRestTest.java
@@ -2,29 +2,23 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberMonthRestTest {
-    @Test
-    public void testCalculateNumberMonthRest1() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/DataTest.csv")
+    public void testCalculateNumberMonthRest1(int expected, int income, int expenses, int threshold) {
         NumberMonthRest service = new NumberMonthRest();
 
-       int actual = service.calculate(10_000, 3_000, 20_000);
-       int expected = 3;
+       int actual = service.calculate(income, expenses, threshold);
+
 
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
-    public void testCalculateNumberMonthRest2() {
-        NumberMonthRest service = new NumberMonthRest();
-
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        int expected = 2;
-
-        Assertions.assertEquals(expected, actual);
-
-    }
 }
